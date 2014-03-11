c3c7f5d72adb758e1a48d377c43c68fb983e5a84
Jpiemaz <jmazzoli@gmail.com>
2014-03-11 16:36:37 -0400

    Add, edit, update, destroy images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,16 +3,33 @@ class ImagesController < ApplicationController
     @image = Image.new
     @gallery = Gallery.find(params[:gallery_id])
   end
+  
   def create
     gallery = Gallery.find(params[:gallery_id])
     image = gallery.images.create(image_params)
     redirect_to gallery
   end
+  
+  def show
+    @image = Image.find(params[:id])
+  end
+  
+  def edit
+    @image = Image.find(params[:id])
+  end
+  
+  def update
+    image = Image.find(params[:id])
+    image.update(image_params)
+    redirect_to image
+  end
+  
   def destroy
     image = Image.find(params[:id])
     image.destroy
-    redirect_to gallery
+    redirect_to image.gallery
   end
+  
   private
   
   def image_params
