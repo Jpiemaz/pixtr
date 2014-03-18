dfbede1cedb37821e5b3fa4293e225cf010489ba
John  Mazzoli <johnmazzoli@Macintosh-2.local>
2014-03-18 17:04:23 -0400

    Add groups

---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ActiveRecord::Base
+  has_many :group_memberships
+  has_many :members, through: :group_memberships
+  validates :name, presence: true 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Pixtr::Application.routes.draw do
     resources :images, only: [:new, :create]
   end
   
+  resources :groups, only: [:index, :new, :create, :show]
+  
   resources :images, except: [:index, :new, :create] do
     resources :comments, only: [:create]
   end
